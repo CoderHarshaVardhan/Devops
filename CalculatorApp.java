580c5a7516062c719428cdc2c50d977bdee89a57
Harsha <2004.Harsha.VArdhan.2004@gmail.com>
2024-10-27 16:34:57 +0530

    Calculator app

---
+++ CalculatorApp.java
@@ -0,0 +1,12 @@
+public class CalculatorApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a = 10;
+        int b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
